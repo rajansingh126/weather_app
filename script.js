d31d4921271ae05bf2b0a03e85472176bd92a7e1
rajansingh126 <rajansingh895876@gmail.com>
2023-08-14 21:22:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const usertab=document.querySelector("[data-userwether]");
+const searchtab=document.querySelector("[data-searchweather]");  
+const usercondition=document.querySelector(".wether-container");
+const foramsearch=document.querySelector(".foram-container");
+const useracces=document.querySelector("[ Data-gantAcess]"); 
+const userinfo=document.querySelector("[user-info-container]");
